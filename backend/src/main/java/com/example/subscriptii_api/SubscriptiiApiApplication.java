6695e12083c306613b5f953bd8aaa21d1eb0b182
Carina <carinacoltan29@gmail.com>
2026-03-25 14:00:57 +0200

    Tech Lead: Initializat schelet backend Spring Boot

---
+++ backend/src/main/java/com/example/subscriptii_api/SubscriptiiApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.subscriptii_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptiiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptiiApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/subscriptii_api/SubscriptiiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subscriptii_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptiiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
